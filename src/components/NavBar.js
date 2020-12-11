d3a21d125888ef80728e816acf7b51a4cd816935
Christian Ares <christianaresvillan@gmail.com>
2020-12-11 13:36:14 +0100

    starting with styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import AuthService from './auth/auth-service';
 import './NavBar.css'
-// import Navbar from 'react-bootstrap/Navbar'
-// import Button from 'react-bootstrap/Button'
-// import FormControl from 'react-bootstrap/FormControl'
-// import Form from 'react-bootstrap/Form'
-// import NavDropdown from 'react-bootstrap/DropdownMenu'
-// import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button'
+import FormControl from 'react-bootstrap/FormControl'
+import Form from 'react-bootstrap/Form'
+import NavDropdown from 'react-bootstrap/DropdownMenu'
+import Nav from 'react-bootstrap/Nav'
 
 
 class NavBar extends Component {
@@ -78,6 +78,23 @@ class NavBar extends Component {
             <li><Link to='/signup'>Acceso Padres</Link></li>
             <li><Link to='/login'>Iniciar Sesión</Link></li>
           </ul>
+{/* <Navbar fixed="top" bg="light" expand="lg">
+   <Navbar.Brand href="https://github.com/Christian-Ares">Logo</Navbar.Brand>
+   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+   <Navbar.Collapse id="basic-navbar-nav">
+     <Nav className="mr-auto">
+       <Nav.Link href="/">Home</Nav.Link>
+       <Nav.Link href="/login">Link</Nav.Link>
+       <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+         <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+       </NavDropdown>
+     </Nav>
+  </Navbar.Collapse>
+ </Navbar> */}
         </nav>
       )
     }
