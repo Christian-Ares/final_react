90fdc34d7afa6aeb0663efaf479ea4a5e56a9d94
Christian Ares <christianaresvillan@gmail.com>
2020-12-18 13:09:13 +0100

    Styles

--- src/components/Servicios.js
+++ src/components/Servicios.js
@@ -10,18 +10,20 @@ const Servicios = () => {
 
         <br/>
 
-        <div class="card">
-        <h2>Servicio de comedor</h2>
-        <p>Ofrecemos un servicio de comedor para las familias que tienen una jornada laboral larga, donde los niños puedan comer de forma saludable, todo asesorado por una nutricionista externa, y con todos los servicios de cocina integrados en el centro</p>
+        <div class="services-card">
+          <div class="services-content">
+            <h2>Servicio de comedor</h2>
+            <p>Ofrecemos un servicio de comedor para las familias que tienen una jornada laboral larga, donde los niños puedan comer de forma saludable, todo asesorado por una nutricionista externa, y con todos los servicios de cocina integrados en el centro</p>
+          </div>
         </div>
-
         <br/>
 
-        <div class="card">
-        <h2>Servicio de "Buenos días cole"</h2>
-        <p>Ofrecemos un servicio de buenos días cole, para aquellas familias que comiencen su jornada laboral temprano, para así poder dejar a sus peques en la escuela desde primera hora, el horario de este servicio comienza a las 7:00 AM</p>
+        <div class="services-card">
+          <div class="services-content">
+            <h2>Servicio de "Buenos días cole"</h2>
+            <p>Ofrecemos un servicio de buenos días cole, para aquellas familias que comiencen su jornada laboral temprano, para así poder dejar a sus peques en la escuela desde primera hora, el horario de este servicio comienza a las 7:00 AM</p>
+          </div>
         </div>
-        
       </div>
     </div>
   )
